b7d5d6fd0f08d4b74ea1b5a0b9d214be72622c56
Vibhuti Narayan singh <narayansinghvibhuti2@gmail.com>
2025-01-16 19:44:08 +0530

    ContextSetup

---
+++ src/context/CartContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react"
+
+const initialState = {
+    cartList: [],
+    total: 0
+}
+
+const CartContext = createContext(initialState);
+
+export const CartProvider = ({children}) => {
+    const value = {
+        total: 50,
+    };
+    return (
+        <CartContext.Provider value={value}>
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+export const useCart = () => {
+    return useContext(CartContext);
+}
+
